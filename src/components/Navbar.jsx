a03299698e509271b15b32e8f6c7e3b35be19db0
Matias Blanc <programador.matiasblanc@gmail.com>
2022-04-14 15:28:17 -0400

    added contact section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
             <li className="navbar__element"><a href="#header">Inicio</a></li>
             <li className="navbar__element"><a href="#aboutme">Sobre Mi</a></li>
             <li className="navbar__element"><a href="#project">Proyectos</a></li>
-            <li className="navbar__element">Contacto</li>   
+            <li className="navbar__element"><a href="#contact">Contacto</a></li>
         </ul>
     </nav>
 )
